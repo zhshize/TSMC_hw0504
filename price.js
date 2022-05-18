c107c8bcb21b371de1711e7c2ac15f6fde797749
Shih-Tse Chang <stchang@cs.nycu.edu.tw>
2022-05-18 19:27:48 +0800

    Pass simple discount test

--- price.js
+++ price.js
@@ -1,16 +1,40 @@
 function price(books) {
     if (books.length === 0) return 0;
     if (books.length === 1) return 8;
-    if (books.length) {
-        const b = books[0];
-        let diff = false;
-        for (const book of books) {
-            if (book !== b) diff = true;
-        }
-        if (!diff) return books.length * 8;
+    const counts = [0, 0, 0, 0, 0];
+    for (const book of books) {
+        counts[book]++;
     }
-    if (books.length === 1) return 8;
-    return 0;
+    let p = 0;
+
+    while(true) {
+        let c = 0;
+        for (let i = 0; i < 5; i++) {
+            if (counts[i] > 0) {
+                counts[i]--;
+                c++;
+            }
+        }
+        switch (c) {
+            case 5:
+                p += 8 * 5 * 0.75;
+                break;
+            case 4:
+                p += 8 * 4 * 0.8;
+                break;
+            case 3:
+                p += 8 * 3 * 0.9;
+                break;
+            case 2:
+                p += 8 * 2 * 0.95;
+                break;
+            case 1:
+                p += 8;
+                break;
+            case 0:
+                return p;
+        }
+    }
 }
 
 module.exports = price;
