377e8bc14e5a378f769d34e8f1bc493f858f2560
Shih-Tse Chang <stchang@cs.nycu.edu.tw>
2022-05-18 16:41:42 +0800

    Pass basic test

--- price.js
+++ price.js
@@ -1,5 +1,16 @@
 function price(books) {
-    return 'to be implemented'
+    if (books.length === 0) return 0;
+    if (books.length === 1) return 8;
+    if (books.length) {
+        const b = books[0];
+        let diff = false;
+        for (const book of books) {
+            if (book !== b) diff = true;
+        }
+        if (!diff) return books.length * 8;
+    }
+    if (books.length === 1) return 8;
+    return 0;
 }
 
 module.exports = price;
